36a73117bad8bfc4946b964975dc57824bf4d415
Susanoo-no-Mikoto <19300707@live.inueco.ru>
2023-03-29 20:49:46 +0500

    1.5

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
+server.use(auth);
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
